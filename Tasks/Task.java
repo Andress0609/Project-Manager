99a735a37d6aebf30f1fad432e0669cba3c04dae
Andres <anrodriguezbr@est.utn.ac.cr>
2024-08-09 15:03:19 -0600

    Se crea clase de Tasks
    Clase task

---
+++ Tasks/Task.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Tasks;
+
+import Users.User;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author andre
+ */
+public class Task {
+    private int id;
+    private String descripcion;
+    private User Users;
+    private boolean completado;
+    private List commentList;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public User getUsers() {
+        return Users;
+    }
+
+    public boolean isCompletado() {
+        return completado;
+    }
+
+    public List getCommentList() {
+        return commentList;
+    }
+
+    public Task(int id, String descripcion, User Users, boolean completado) {
+        this.id = id;
+        this.descripcion = descripcion;
+        this.Users = Users;
+        this.completado = completado;
+        this.commentList = new ArrayList();
+    }
+
+    public Task() {
+        this(0,"",new User(),false);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" + "id=" + id + ", descripcion=" + descripcion + ", Users=" + Users + ", completado=" + completado + ", commentList=" + commentList + '}';
+    }
+   
+    
+
+}
